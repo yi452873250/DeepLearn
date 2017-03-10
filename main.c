ae17ed9dfb4a2e4e07f85d0fab261a904d0406bc
yizhibin <452873250@qq.com>
2017-03-10 10:12:30 +0800

    insert main.cpp

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	int a=0;
+	int b=10;
+	if(a+2>10)
+	{
+		printf("hello world\n");
+	}
+	else
+	{
+		printf("this is a error \n");
+
+	}
+	int c=b*b-4*a*c;
+	if(c<0)
+	{
+		printf("this is uncheckable\n");
+	}
+	else
+	{
+		printf("it has two values\n");
+
+	}
+	return 0;
+	system("pause");
+}
